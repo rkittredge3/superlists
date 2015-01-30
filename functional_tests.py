866d53f0747b3cc54abf948bc891cfb458bd054e
Rob Kittredge <rkittredge@partners.org>
2015-01-30 16:14:09 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,20 +15,37 @@ class NewVisitorTest(unittest.TestCase):
         # Wendy has heard about an online to-do list site. She goes to it's home page using her Web browser.
         self.browser.get('http://localhost:8000')
 
-        # The home page tells her she's looking at the To-Do list site.
+        # The home page title, and a heading, tells her she's looking at the To-Do list site.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('...more steps need to be added to this test!')
 
         # The home page has a text box that invites her to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # She types "Buy peacock feathers". (She likes to make fly-fishing lures.)
-
+        inputbox.send_keys('Buy peacock feathers')
         # She hits enter and the page updates.
+        inputbox.send_keys(Keys.ENTER)
+        
         # Now the page lists "1: Buy peacock feathers"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item.
+        self.fail('Finish the test!')
+        
         # She types "Use peacock feathers to make a fly"
 
         # She hits enter and the page updates again.
+        
         # Now the page lists both of her items.
 
         # Wendy notices that the site has generated a unique URL for her list, with some explanatory text to that effect.
