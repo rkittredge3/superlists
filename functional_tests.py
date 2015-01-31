0ac91b5339d0e5cbbd5c480fac0eef531fb9036e
Rob Kittredge <rkittredge@partners.org>
2015-01-31 00:52:49 -0500

    Now last POSTed to-do item appears in table

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -28,30 +29,41 @@ class NewVisitorTest(unittest.TestCase):
         )
         # She types "Buy peacock feathers". (She likes to make fly-fishing lures.)
         inputbox.send_keys('Buy peacock feathers')
+        time.sleep(1)
         # She hits enter and the page updates.
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         # Now the page lists "1: Buy peacock feathers"
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         
         # She types "Use peacock feathers to make a fly"
-
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        time.sleep(1)
         # She hits enter and the page updates again.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         # Now the page lists both of her items.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Wendy notices that the site has generated a unique URL for her list, with some explanatory text to that effect.
         # She visits that URL and sees that her list is still there.
 
         # She calls it a day.
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
