0f680faf6d2ed5518593109d7790fac2f25f0a18
Rob Kittredge <rkittredge@partners.org>
2015-02-16 15:14:06 -0500

    Hack FR runner to be able to use staging URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,26 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 import time
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Look for 'liveserver' argument and, if found, use that as our test server url.
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        # If 'liveserver' argument is omitted, just use built-in test server url.
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about an online to-do list site. She goes to it's home page using her Web browser.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # The home page title, and a heading, tells her she's looking at the To-Do list site.
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('peacock feathers', page_text)
 
@@ -86,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
 
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         window_width = 1024
         window_height = 768
         self.browser.set_window_size(window_width, window_height)
